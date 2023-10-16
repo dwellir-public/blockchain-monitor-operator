ab33c086d56a55994029c22415d2799b0ccc10ce
Jakob Andersson <erikjakobandersson@gmail.com>
2023-10-16 14:19:27 +0200

    Improve debug logging

--- templates/monitor-blockchains.py
+++ templates/monitor-blockchains.py
@@ -28,9 +28,9 @@ def main():
 
     # Set up logging
     # TODO: make log level load from config
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
     console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setLevel(logging.INFO)
+    console_handler.setLevel(logging.DEBUG)
     console_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
     logger.addHandler(console_handler)
 
@@ -63,11 +63,12 @@ def main():
     program_counter = {'loop_time': [], 'failed_requests': []}
     while True:
         logger.info("- MONITOR LOOP START")
-        loop_start_time = time.time()
-        # TODO: add logging outliers, std, to catch timeouts?
+        time_loop_start = time.time()
         all_endpoints = load_endpoints(config['RPC_FLASK_API'], cache_max_age)
-        # all_results = loop.run_until_complete(fetch_results(all_endpoints))
-        all_results = fetch_results_pycurl(endpoints=all_endpoints)
+        time_endpoints_loaded = time.time()
+        concurrent_connections = 8
+        all_results = fetch_results_pycurl(endpoints=all_endpoints, num_connections=concurrent_connections)
+        time_results_fetched = time.time()
 
         # Create block_heights dict
         block_heights = {}
@@ -85,7 +86,6 @@ def main():
                     block_heights[chain].append((url, result.get('latest_block_height')))
             except (AttributeError, UnboundLocalError) as e:
                 logger.error(f'{e.__class__.__name__} for {result}, %s', e)
-
         # Calculate block_height diffs and maxes
         block_height_diffs = {}
         chain_max_heights = {}
@@ -96,14 +96,16 @@ def main():
             chain_max_heights[chain] = max_height
             for rpc in rpc_list:
                 block_height_diffs[chain][rpc[0]] = max_height - rpc[1]
+        time_block_calc_done = time.time()
 
         timestamp = datetime.utcnow()
         records = []
-        loop_counter = {'failed_requests': 0, 'http': 0, 'ws': 0}
+        loop_counter = {'failed_requests': 0, 'http': 0, 'ws': 0, 'times_http': [], 'times_ws': []}
         # TODO: do result loop by chain, and set timestamp per chain
         # Create and append RPC data points
         for result in all_results:
             if result:
+                time_result_start = time.time()
                 try:
                     try:
                         http_code = result.get('http_code', -2)
@@ -112,10 +114,10 @@ def main():
                     except KeyError as e:
                         logger.error("KeyError when accessing result [%s], error: [%s]", result, e)
                         continue
-                    if 'ws' in url:
-                        loop_counter['ws'] = loop_counter['ws'] + 1
                     if 'http' in url:
                         loop_counter['http'] = loop_counter['http'] + 1
+                    if 'ws' in url:
+                        loop_counter['ws'] = loop_counter['ws'] + 1
                     # TODO: clean up the point creation
                     if http_code != 200 and url not in block_height_diffs[chain]:
                         logger.warning("HTTP code [%s] for %s, something went wrong with the request.", http_code, url)
@@ -141,8 +143,14 @@ def main():
                 except Exception as e:
                     logger.error("%s while accessing results for [%s], results: [%s], error: [%s]", {
                                  e.__class__.__name__}, url, result, str(e))
+                time_result_end = time.time()
+                if 'http' in url:
+                    loop_counter['times_http'].append(time_result_end - time_result_start)
+                if 'ws' in url:
+                    loop_counter['times_ws'].append(time_result_end - time_result_start)
             else:
                 logger.warning("Couldn't get results from [%s]. Skipping.", url)
+        time_results_parsed = time.time()
 
         # Create and append max block height data points
         for chain, max_height in chain_max_heights.items():
@@ -151,8 +159,11 @@ def main():
                            .tag("url", "Max over time")
                            .field("block_height", max_height)
                            .time(timestamp))
+        write_to_influxdb(influxdb['url'], influxdb['token'], influxdb['org'], influxdb['bucket'], records)
+        time_influxdb_written = time.time()
+
         logger.info("- MONITOR LOOP END")
-        loop_time = time.time() - loop_start_time
+        loop_time = time.time() - time_loop_start
         logger.info("Loop - Successful requests:  %s/%s", len(all_results), len(all_endpoints))
         logger.info("Loop - Failed requests:      %s", loop_counter['failed_requests'])
         logger.info("Loop - Endpoints using http: %s", loop_counter['http'])
@@ -164,8 +175,21 @@ def main():
         logger.info("Program - Loops since program start: %s", len(program_counter['loop_time']))
         logger.info("Program - Mean loop processing time: %.3fs", mean(program_counter['loop_time']))
         logger.info("Program - Average failed requests:   %.2f", mean(program_counter['failed_requests']))
-        logger.info("Writing to InfluxDB")
-        write_to_influxdb(influxdb['url'], influxdb['token'], influxdb['org'], influxdb['bucket'], records)
+
+        # Debugging info
+        endpoints_load_time = time_endpoints_loaded - time_loop_start
+        fetch_results_time = time_results_fetched - time_endpoints_loaded
+        block_calc_time = time_block_calc_done - time_results_fetched
+        parse_results_time = time_results_parsed - time_block_calc_done
+        write_influx_time = time_influxdb_written - time_results_parsed
+
+        logger.debug("Info - Concurrent connections: %s", concurrent_connections)
+        logger.debug("Time data - Loading endpoints: %.3fs", endpoints_load_time)
+        logger.debug("Time data - Fetching results: %.3fs", fetch_results_time)
+        logger.debug("Time data - Block calculations: %.3fs", block_calc_time)
+        logger.debug("Time data - Parse results: %.3fs", parse_results_time)
+        logger.debug("Time data - Write to InfluxDB: %.3fs", write_influx_time)
+
         logger.info("Sleeping for %s seconds...", request_interval)
         # Sleep between making requests to avoid triggering rate limits.
         time.sleep(request_interval)
@@ -441,8 +465,10 @@ def fetch_results_pycurl(endpoints: list, num_connections: int = 4) -> list:
                 logger.debug("Success for URL: [%s], msg: [%s]", c.url, c.getinfo(pycurl.EFFECTIVE_URL))
                 freelist.append(c)
             for c, errno, errmsg in err_list:
-                logger.debug("Failed curl for URL: [%s], err-num: [%s], err-msg: [%s]", c.url, errno, errmsg)
+                logger.debug("Failed curl for URL: [%s], err-num: [%s], err-msg: [%s]. Making a websocket request instead!",
+                             c.url, errno, errmsg)
                 # TODO: implement a better way of handling websockets, this is only a "retry if fail"
+                # TODO: keep parallelization in solution!
                 if errno == 1 and "wss" in errmsg:
                     try:
                         wss_block_height, wss_http_code = make_ws_request(c.url, c.api_class)
