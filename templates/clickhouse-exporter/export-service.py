9548e40b5bcdeab88695cc081c51955993a7bcc0
Jakob Ersson <11717405+jakobilobi@users.noreply.github.com>
2025-02-05 11:39:11 +0100

    feat: initial version of service script

---
+++ templates/clickhouse-exporter/export-service.py
@@ -0,0 +1,47 @@
+"""Utilize the exporter logic to recurringly export data from BCM's InfluxDB to ClickHouse.
+
+Should be run in a service in the same container as BCM, hence why it is included in the charm.
+"""
+
+import asyncio
+import logging
+
+import schedule
+from exporter import load_exporter_config
+
+
+def enabled() -> bool:
+    """Check if the exporter is enabled."""
+    return load_exporter_config().get("enabled", False)
+
+
+async def init():
+    """Initialize the exporter service."""
+    logging.info("Initializing exporter service...")
+    # TODO: implement
+
+
+async def main():
+    """Schedule exporting every 15 minutes."""
+    # Initialize the exporter
+    await init()
+
+    # Execution time is in UTC
+    # TODO: set task
+    # TODO: set interval from config
+    schedule.every(15).minutes.do(None)
+
+    # Run the first iteration immediately
+    # TODO: implement
+
+    while True:
+        schedule.run_pending()
+        await asyncio.sleep(60)  # Sleep to prevent busy waiting
+
+
+if __name__ == "__main__":
+    if not enabled():
+        logging.info("Exporter is disabled.")
+        exit(0)
+
+    asyncio.run(main())
