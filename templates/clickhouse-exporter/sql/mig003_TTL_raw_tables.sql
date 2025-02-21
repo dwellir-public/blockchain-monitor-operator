6e528fd39b0f4e00738b9b99c8a10a4291da3aa6
Jakob Ersson <11717405+jakobilobi@users.noreply.github.com>
2025-02-21 12:14:13 +0100

    fix: change TTL from 45 days to 1 year

--- templates/clickhouse-exporter/sql/mig003_TTL_raw_tables.sql
+++ templates/clickhouse-exporter/sql/mig003_TTL_raw_tables.sql
@@ -2,9 +2,19 @@
 ALTER TABLE
     block_height_requests
 MODIFY
-    TTL timestamp + INTERVAL 45 DAY;
+    TTL timestamp + INTERVAL 1 YEAR;
 
 ALTER TABLE
     max_height_over_time
 MODIFY
-    TTL timestamp + INTERVAL 45 DAY;
+    TTL timestamp + INTERVAL 1 YEAR;
+
+ALTER TABLE
+    system.text_log
+MODIFY
+    TTL event_time + INTERVAL 2 DAY;
+
+ALTER TABLE
+    system.trace_log
+MODIFY
+    TTL event_time + INTERVAL 2 DAY;
